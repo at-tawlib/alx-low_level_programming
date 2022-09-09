04442b20f1f8466124e42985082466babd7bdb57
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-09-08 20:20:18 -0700

    Use string literals in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fprintf(stderr, text);
+const char *t = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fprintf(stderr, t);
 return (1);
 }
