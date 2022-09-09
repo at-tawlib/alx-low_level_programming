95ad5141367e842fb80747ad9374837ff06f375a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-09-08 20:11:56 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fprintf(stderr, text);
 return (1);
 }
