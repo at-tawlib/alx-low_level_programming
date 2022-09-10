a3f6f2fa3650a77a0b7a89f16065bc3621e835c2
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-09-09 22:29:45 -0700

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+int a, b, n;
+for (a = 97, n = 1; n < 27; a++, n++)
+{
+putchar(a)
+}
+for (b = 65, n = 1; n < 27; b++, n++)
+{
+putchar(b);
+}
+putchar(10);
+return (0);
+}
