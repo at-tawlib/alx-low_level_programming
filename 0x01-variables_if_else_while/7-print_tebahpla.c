15a0c11961b897efebb7bc4847a987fda85ef7ed
at-tawlib <a.fatahu95@gmail.com>
2022-09-12 15:33:42 +0000

    Change for to while loop

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - print lowercase in reverse
- * ASCII values (97-122) for lowercase (a-z)
- * Return: 0 for success
+ * main - print lowercase alphabet except q and e
+ * we use ASCII 97-122(a - z)
+ * Return: 0 if success
  */
 int main(void)
 {
-int ascii;
-for (ascii = 122; ascii >= 97; ascii--)
+int alph = 122;
+while (alph >= 97)
 {
-putchar(ascii);
+putchar(alph);
+alph--;
 }
-putchar('\n');
+putchar(10);
 return (0);
 }
