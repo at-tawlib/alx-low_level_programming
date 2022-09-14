bdac6fb31e44ef6d9ca85d849bea186d0241ff4d
at-tawlib <a.fatahu95@gmail.com>
2022-09-14 19:26:33 +0000

    Add main.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - beginning of function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	/* print_alphabet(); */
+	/* print_alphabet_x10(); */
+
+	/*
+	int r;
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+	*/
+
+	/*
+	int r;
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
+	*/
+
+	/*
+	int r;
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+	*/ 
+
+	/*
+	int r;
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+	*/ 
+
+
+	/*
+	int r;
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    _putchar('0' + r);
+    _putchar('\n');
+	*/
+
+
+	/* jack_bauer(); */
+
+	/*
+	times_table();
+	*/
+
+	/*
+	int n;
+	n = add(89,9);
+	printf("%d\n",n);
+	*/
+
+
+	/*
+	print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+	*/
+
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+
+	return (0);
+}
