1a9055b439359d1341c9e8953e776f32ef43871f
at-tawlib <a.fatahu95@gmail.com>
2022-09-14 17:31:16 +0000

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print first 50 Fibonacci numbers
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int count = 0;
+	long int previous = 1;
+	long int next = 1;
+	long int sum = 1;
+
+	while (count < 50)
+	{
+		printf("%lu", sum);
+		previous = sum;
+		sum = sum + next;
+		next = previous;
+
+		if (count < 49)
+		{
+			printf(", ");
+		}
+		count++;
+	}
+
+	return (0);
+}
