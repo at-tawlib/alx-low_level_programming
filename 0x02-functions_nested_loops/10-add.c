42380ffe159668e78eb5a169ce25f2ae5dc5996e
at-tawlib <a.fatahu95@gmail.com>
2022-09-14 00:24:53 +0000

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers and returns the result
+ * @a: first int value
+ * @b: second int value
+ * Return: results of a+b
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
