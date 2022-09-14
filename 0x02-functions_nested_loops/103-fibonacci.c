47c7b73cd98492e6207bddfc4c4483342d389af4
at-tawlib <a.fatahu95@gmail.com>
2022-09-14 18:05:17 +0000

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int count = 0;
+	long int previous = 1;
+	long int next = 1;
+	long int sum = 1;
+	long int sumOfEven = 0;
+
+	while (sum < 4000000)
+	{
+		if (sum % 2 == 0)
+		{
+			sumOfEven = sumOfEven + sum;
+		}
+
+		previous = sum;
+		sum = sum + next;
+		next = previous;
+	}
+
+	printf("%lu\n", sumOfEven);
+	return (0);
+}
