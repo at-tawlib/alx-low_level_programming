09d0d3cbf1c66fc89d791909a23ae47ad0ff624e
at-tawlib <a.fatahu95@gmail.com>
2022-09-15 14:00:18 +0000

    Create 5-moe_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int count = 0;
+
+	while (count <= 10)
+	{
+		int n = 0;
+
+		while (n <= 14)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			n++;
+		}
+		count++;
+		_putchar(10);
+	}
+}
