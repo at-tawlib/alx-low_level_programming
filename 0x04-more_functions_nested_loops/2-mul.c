2bb47be198ed3a1ec0da06700934649f5f430932
at-tawlib <a.fatahu95@gmail.com>
2022-09-15 13:56:40 +0000

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - multiplies two integers
+ * @a: integer to multiply
+ * @b: integer to multiply
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
