cdab0e0d14e72f742eb0988d186777b723c2b603
at-tawlib <a.fatahu95@gmail.com>
2022-09-15 13:57:54 +0000

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		_putchar(n + '0');
+		n++;
+	}
+	_putchar(10);
+}
