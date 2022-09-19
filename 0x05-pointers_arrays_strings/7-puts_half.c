b6cc5d9a2f075a091957ed8344ddc7be63b3b503
at-tawlib <a.fatahu95@gmail.com>
2022-09-19 22:54:45 +0000

    Create 7-puts_half.c and update main.h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int len;
+	int half;
+
+	/** get length of string **/
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	if (len % 2 == 0)
+		half = len / 2;
+	else
+		half = (len - 1) / 2;
+
+	while (half < len)
+	{
+		_putchar(half + '0');
+		half++;
+	}
+	_putchar(10);
+
+}
