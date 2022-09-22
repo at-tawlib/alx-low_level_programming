27ac73454f39ce8d70a00343d3abc1b1c7bb7b7a
at-tawlib <a.fatahu95@gmail.com>
2022-09-22 10:33:18 +0000

    Add 3-strcmp.c and main.c, update README and main.h

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,8 @@
 # 0x06. C - More pointers, arrays and strings
+
+**0-strcat.c** : function that concatenates two strings
+
+**1-strncat.c** : function that concatenates two strings of `n` bytes
+
+**2-strncpy.c** : function that copies a string without using the standard libraries. 
+>Works exactly like `strncpy`
---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point to test functions
+ * Return: always 0
+ */
+int main(void)
+{
+	/** 0-stcat.c **/
+	/*
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	*/
+
+	/** 1-strncat.c **/
+	/*
+	char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+	*/
+
+	/** 2-strncpy.c **/
+	/**
+	char s1[98];
+    char *ptr;
+    int i;
+
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+	*/
+
+	/** 3-strcmp.c **/
+	char s1[] = "Hello";
+	char s2[] = "World!";
+
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+
+
+	return (0);
+}
