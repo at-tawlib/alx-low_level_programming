b01a73c4e552da5f07b8d4656da58ddc1db1353b
at-tawlib <a.fatahu95@gmail.com>
2022-09-22 01:41:39 +0000

    Add 1-strncat.c and update main.h

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncat - concatenates two strings of n bytes
+ * @dest: string to concatenate to
+ * @src: string to use for concatination
+ * @n: number of bytes to use
+ * Return: concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	len = i;
+	for (i = 0; i < n && src[1] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+
+	dest[len + i] = '\0';
+	return (dest);
+}
