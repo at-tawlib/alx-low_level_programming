44e128a3b09a27de73990a094b6f1e35cb4d7783
at-tawlib <a.fatahu95@gmail.com>
2022-09-23 01:51:45 +0000

    Change int to unsigned int

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,12 +5,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, temp;
+	unsigned int i, temp;
 
-	for (i = 0; i < n / 2; i++, n--)
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = temp;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }
