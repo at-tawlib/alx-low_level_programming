2d45ab68b5563278ef20c9f6423163f6e91b105a
at-tawlib <a.fatahu95@gmail.com>
2022-09-23 04:08:49 +0000

    Replace if statements with for and while loop

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,24 +5,21 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char letters[] = "aeotlAEOTL";
+	char code[] = "4307143071";
 
+	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-
-		if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-
-		if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-
-		if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-
-		if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = code[j];
+				break;
+			}
+		}
 
 		i++;
 	}
