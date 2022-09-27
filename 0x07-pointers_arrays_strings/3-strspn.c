1447d1d34907b555d6bb7426c991c669e76b6f33
at-tawlib <a.fatahu95@gmail.com>
2022-09-27 00:41:18 +0000

    Add 2-strspn.c and update main.h

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+/**
+ * _strspn - returns number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, len;
+
+	len = 0;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		j = 0;
+		while (s[j] != '\0')
+		{
+			if (accept[i] == s[j])
+			{
+				len++;
+				break;
+			}
+			j++;
+		}
+	}
+
+	return (len);
+}
