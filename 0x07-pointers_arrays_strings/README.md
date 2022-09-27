37d35a4238995c9038a48c28ed2c4ef233510906
at-tawlib <a.fatahu95@gmail.com>
2022-09-27 08:03:44 +0000

    Update README.md and main.c

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,17 @@
 # 0x07.C - Even more pointers, arrays and strings
+
+**0-memset.c** : function fills the first `n` bytes of the memory are pointed to by `s` with constant byte `b` and returns a pointer to the memory area `s` (works like the `memset` function of the standard library
+
+**1-memcpy.c** : copies bytes from memory area to destination and returns pointer to destination. (Works like the `memcpy()` function).
+
+**2-strchr.c** : returns a pointer to the first occurance of a character in the string given or `NULL` is character is not found
+
+**3-strspn.c** : function that gets the length of a prefix substring
+
+**4-strpbrk.c** : searches a string for any of a set of bytes
+
+**5-strstr.c** : locates a substring in a string
+
+**7-print_chessboard.c** : prints a chessboard
+
+**8-print_diagsums.c** : prints the sum of the two diagonals of a square matrix of integers
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -8,6 +8,8 @@
  */
 int main(void)
 {
+	/** 2-strchr.c */
+	/**
 	char *s = "hello";
 	char *f;
 
@@ -17,6 +19,68 @@ int main(void)
 	{
 		printf("%s\n", f);
 	}
+	*/
+
+	/** 3-strspn.c */
+	/**
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
+
+	n = _strspn(s, f);
+	printf("%u\n", n);
+	*/
+
+	/** 4-strpbrk.c */
+	/**
+	char *s = "hello, world";
+	char *f = "world";
+	char *t; 
+
+	t = _strpbrk(s, f);
+	printf("%s\n", t);
+	*/
+
+	/** 5-strstr.c */
+	/**
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	*/
+
+	/** 7-print_chessboard.c **/
+	/**
+	    char board[8][8] = {
+        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+    };
+    print_chessboard(board);
+	*/
+	
+	/** 8-pring_diagsums.c **/
+	 int c3[3][3] = {
+        {0, 1, 5},
+        {10, 11, 12},
+        {1000, 101, 102},
+    };
+    int c5[5][5] = {
+        {0, 1, 5, 12124, 1234},
+        {10, 11, 12, 123521, 12512},
+        {1000, 101, 102, 12545, 214543435},
+        {100, 1012451, 11102, 12545, 214543435},
+        {10, 12401, 10452, 11542545, 1214543435},
+    };
+    print_diagsums((int *)c3, 3);
+    print_diagsums((int *)c5, 5);
 
 	return (0);
 }
