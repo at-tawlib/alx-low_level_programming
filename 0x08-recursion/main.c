78bcc1dd4dbd6a19e4084a56befcf987942daed5
at-tawlib <a.fatahu95@gmail.com>
2022-09-29 05:55:27 +0000

    Update main.c

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -8,6 +8,93 @@
  */
 int main(void)
 {
+	/** 0-puts_recursion.c **/
+	/*
     _puts_recursion("Puts with recursion");
+	*/
+
+	/** 1-print_rev_recursion.c */
+	/*
+	_print_rev_recursion("\nColton Walker");
+	*/
+
+	/** 2-strlen_recursion.c */
+	/**
+	int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+	*/
+
+	/** 3-factorial.c */
+	/**
+	int r;
+
+    r = factorial(1);
+    printf("%d\n", r);
+    r = factorial(5);
+    printf("%d\n", r);
+    r = factorial(10);
+    printf("%d\n", r);
+    r = factorial(-1024);
+    printf("%d\n", r);
+	*/
+
+	/** 4-pow_recursion.c */
+	/**
+	int r;
+
+    r = _pow_recursion(1, 10);
+    printf("%d\n", r);
+    r = _pow_recursion(1024, 0);
+    printf("%d\n", r);
+    r = _pow_recursion(2, 16);
+    printf("%d\n", r);
+    r = _pow_recursion(5, 2);
+    printf("%d\n", r);
+    r = _pow_recursion(5, -2);
+    printf("%d\n", r);
+    r = _pow_recursion(-5, 3);
+    printf("%d\n", r);
+	*/
+
+	/** 5-sqrt_recursion.c */
+	/**
+    int r;
+
+    r = _sqrt_recursion(1);
+    printf("%d\n", r);
+    r = _sqrt_recursion(1024);
+    printf("%d\n", r);
+    r = _sqrt_recursion(16);
+    printf("%d\n", r);
+    r = _sqrt_recursion(17);
+    printf("%d\n", r);
+    r = _sqrt_recursion(25);
+    printf("%d\n", r);
+    r = _sqrt_recursion(-1);
+    printf("%d\n", r);
+	*/
+
+	/** 6-is_prime_number.c */
+    int r;
+
+    r = is_prime_number(1);
+    printf("%d\n", r);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
+    printf("%d\n", r);
+
     return (0);
 }
