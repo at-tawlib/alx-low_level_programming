052ebc06d21469b4b3a684575d3a33fe24835289
at-tawlib <a.fatahu95@gmail.com>
2022-09-29 01:52:50 +0000

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,3 +1,9 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to reverse
+ */
 void _print_rev_recursion(char *s)
 {
 	int i, len;
@@ -10,5 +16,4 @@ void _print_rev_recursion(char *s)
 	{
 		_putchar(s[i]);
 	}
-	_putchar(10);
 }
