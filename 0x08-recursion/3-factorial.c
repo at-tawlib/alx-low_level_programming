a1c86d6ce3ec37d9fc5ff4b268b540ec92e9a4d5
at-tawlib <a.fatahu95@gmail.com>
2022-09-29 02:23:10 +0000

    Add 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number to use for calculation
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
