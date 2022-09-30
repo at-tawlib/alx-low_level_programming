943980f9b79398e71ba9d17253cb2e78ac8ce718
at-tawlib <a.fatahu95@gmail.com>
2022-09-30 19:37:59 +0000

    Recode everything

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * isInteger - checks whether the string is an integer or not
+ * @s: string to check
+ * Return: 0 if is integer and 1 if not integer
+ */
+int isInteger(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+			continue;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/**
 * main -prints results of addition
 * @argc: number of arguments
 * @argv: parameters passed
@@ -9,17 +28,28 @@
 */
 int main(int argc, char *argv[])
 {
-	int sum;
+	int sum, i;
 
-	if (argc == 3)
+	sum = 0;
+
+	if (argc == 1)
 	{
-		sum = atoi(argv[1]) + atoi(argv[2]);
-		printf("%d\n", sum);
+		printf("0\n");
 		return (0);
 	}
 	else
 	{
-		printf("Error\n");
-		return (1);
+		for (i = 1; i < argc; i++)
+		{
+			if (isInteger(argv[i]))
+				sum += atoi(argv[i]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
 	}
 }
