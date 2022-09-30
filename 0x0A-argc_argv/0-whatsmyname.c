184b5629c33837d599a0e2ed10b91dc1ccaedfa4
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-09-30 02:18:11 -0700

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - returns name of program
+* @argc: number of arguments
+* @argv: parameters passed
+* Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
