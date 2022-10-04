b6e213dddfb36b48c19884e766126ad3f43590f0
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-04 04:59:54 -0700

    Add return statement for size == 0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,12 +10,15 @@ char *_strdup(char *str)
 	int i, size;
 	char *new_str;
 
+	if (str == NULL)
+		return (NULL);
 	/** get size of str */
 	for (i = 0; str[i] != '\0'; i++)
 		;
 	size = i;
-
 	new_str = malloc(size * sizeof(char) + 1);
+	if (size == 0)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		new_str[i] = str[i];
 
