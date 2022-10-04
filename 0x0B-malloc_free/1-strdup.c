72b37216e34e838640ce6e175f24cf0c0bdcd871
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-04 05:07:24 -0700

    Move condition of size == 0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,10 +15,10 @@ char *_strdup(char *str)
 	/** get size of str */
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	size = i;
-	new_str = malloc(size * sizeof(char) + 1);
 	if (size == 0)
 		return (NULL);
+	size = i;
+	new_str = malloc(size * sizeof(char) + 1);
 	for (i = 0; i < size; i++)
 		new_str[i] = str[i];
 
