6d8833915aba291277d3af1d7188b656a1797877
at-tawlib <a.fatahu95@gmail.com>
2022-10-05 03:16:48 +0000

    Add 100-argstostr.c, 100-main.c update main.h

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - calculate string length
+ * @s: string to find length
+ * Return: lent of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * argstostr - concatenates all the arguments of main function
+ * @ac: arguments count
+ * @av: arguments
+ * Return: concatenated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, total_length;
+	char *new_str;
+
+	total_length = 0;
+	k = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	/** get total length of all arguments */
+	for (i = 0; i < ac; i++)
+	{
+		total_length += _strlen(av[i]);
+	}
+
+	new_str = malloc(total_length * sizeof(char) + 1);
+	if (new_str == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			new_str[k] = av[i][j];
+			k++;
+		}
+		new_str[k] = '\n';
+		k++;
+	}
+
+	new_str[k] = '\0';
+	return (new_str);
+}
---
+++ 0x0B-malloc_free/100-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - check code of 100-argstostr.c
+ * 
+ * Return: always 0
+ */
+int main(int ac, char *av[])
+{
+	char *s;
+
+	s = argstostr(ac, av);
+	if (s == NULL)
+	{
+		return (1);
+	}
+
+	printf("%s", s);
+	free(s);
+	return (0);
+}
