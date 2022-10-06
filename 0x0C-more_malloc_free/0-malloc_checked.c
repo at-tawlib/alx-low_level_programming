d5cc7605cb1c08338a497c48e919abb5fcc4dcb6
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-06 16:46:32 +0100

    Add code to 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,15 @@
+#include <stdlib.h>
 /**
  * malloc - allocate memeory using malloc
  * @b: size to allocate
  */
 void *malloc_checked(unsigned int b)
 {
+	int *p;
+
+	p = malloc(b);
+	if (p == 0)
+		exit(98);
+
+	return (p);
 }
