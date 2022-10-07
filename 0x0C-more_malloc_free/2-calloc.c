2620c1d90115869b769d9ee6bcc505e0d76e2fd8
at-tawlib <a.fatahu95@gmail.com>
2022-10-07 02:48:25 +0000

    Add code to file + udpate main.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,8 +1,18 @@
+#include <stdlib.h>
 /**
  * _calloc - allocates memory for an array using malloc
- * @nmemb: elements of array
- * @size: size of elements
+ * @nmemb: number of elements of array
+ * @size: size of each element
+ * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *str;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	str = malloc(nmemb * size);
+	if (str == 0)
+		return (NULL);
+	return (str);
 }
