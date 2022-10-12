fe6f05b405a2b0d020854b366a1326068fd37a25
at-tawlib <a.fatahu95@gmail.com>
2022-10-12 21:21:26 +0000

    Add null check and rewrite code

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * int_index - searches for an integer
  * @array: array to search through
@@ -8,14 +9,15 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, value;
+	int i;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		value = cmp(array[i]);
-		if (value != 0)
+		if (cmp(array[i]) != 0)
 			return (i);
 	}
 	return (-1);
