b44ffc6583e8dcc705332b8e9966379fdc42c9a5
at-tawlib <a.fatahu95@gmail.com>
2022-10-13 22:21:37 +0000

    Add struct

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,17 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct print_everything - prints everything
+ * @symbol: symbol representing a data type
+ * @print: function pointer to a function that prints data
+ */
+typedef struct print_everything
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
