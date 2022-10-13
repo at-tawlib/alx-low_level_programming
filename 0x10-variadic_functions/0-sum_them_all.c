cde3a8e2ae98811d1ad2f2fc0a0885a651f9ff93
at-tawlib <a.fatahu95@gmail.com>
2022-10-13 20:10:24 +0000

    Recode file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,19 @@
 #include <stdarg.h>
 
+/**
+ * sum_them_all - returns sum of all its parameters
+ * @n: count
+ * Return: sum of all arguments
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum;
 
-	sum = 0;
 	va_start(ap, n);
-	for (i = n; i != 0; i = va_arg(ap, unsigned int))
-		sum += i;
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 	va_end(ap);
 	return (sum);
 }
