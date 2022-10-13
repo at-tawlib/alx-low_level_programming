1c09d079302fba01e31d4e750aab4fe10ca98522
at-tawlib <a.fatahu95@gmail.com>
2022-10-13 20:59:54 +0000

    Add null check for separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (i < (n - 1))
+		if (i < (n - 1) && separator != NULL)
 			printf("%d%s", va_arg(ap, int), separator);
 		else
 			printf("%d", va_arg(ap, int));
