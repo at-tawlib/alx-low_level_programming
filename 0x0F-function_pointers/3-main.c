83f991dbc98ba194e7f4a9ce3b7d79b6a85d10f4
at-tawlib <a.fatahu95@gmail.com>
2022-10-13 03:47:44 +0000

    Add 3-* files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *operator;
+
+	/** check if arguments are exactly 4 */
+	if (argc != 4)
+	{
+		printf("Error 98\n");
+		exit(98);
+	}
+	/** get numbers and operator */
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+	/** check if the right operator has been entered on operator is not NULL */
+	if (!get_op_func(operator))
+	{
+		printf("Error 99\n");
+		exit(99);
+	}
+	/** check if first number is divided by 0, or modulus of 0 */
+	if (num2 == 0 && (*operator == '/' || *operator == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(operator)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: division of a and b
+ */
+
+int op_div(int a, int b)
+{
+
+	return (a / b);
+}
+
+/**
+ * op_mod - find modulus
+ * @a: first number
+ * @b: second number
+ * Return: remainder of a / b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
