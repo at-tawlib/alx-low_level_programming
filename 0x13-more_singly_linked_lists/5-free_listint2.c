50dc7807c33856ed073e8a33ff1669f282aafbe6
at-tawlib <a.fatahu95@gmail.com>
2022-10-24 21:03:49 +0000

    Add 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and sets head to null
+ * @head: head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node;
+
+	while ((*head) != NULL)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+	}
+}
