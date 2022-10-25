268dade07fa2544fd4152678ac724f5bb1c884b1
at-tawlib <a.fatahu95@gmail.com>
2022-10-25 14:32:14 +0000

    Add 1 to i check of indx

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	for (i = 0; i <= index && cur_node != NULL; i++)
 	{
-		if (i == index)
+		if (i + 1 == index)
 		{
 			prev_node->next = cur_node->next;
 			free(cur_node);
