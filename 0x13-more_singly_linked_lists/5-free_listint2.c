ef59190c415a308e38a3a76c0360016f325dea96
at-tawlib <a.fatahu95@gmail.com>
2022-10-25 06:09:06 +0000

    Set head to NULL at end of code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	if ((*head) == NULL)
-	{
-		free(*head);
-		return;
-	}
 	while ((*head) != NULL)
 	{
 		node = *head;
 		*head = (*head)->next;
 		free(node);
 	}
+	*head = NULL;
 }
