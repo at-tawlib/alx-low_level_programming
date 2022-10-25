b6ab30399fba3e5a6b76e169dfa2eb5a0e31e49b
at-tawlib <a.fatahu95@gmail.com>
2022-10-25 15:10:42 +0000

    Add return in if block

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			new_node->n = n;
 			new_node->next = cur_node;
 			prev_node->next = new_node;
+			return (new_node);
 		}
 
 		prev_node = cur_node;
