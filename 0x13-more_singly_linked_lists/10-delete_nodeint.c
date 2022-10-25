8a19c86e397a4f8f4123aeaf3bdfc559df300366
at-tawlib <a.fatahu95@gmail.com>
2022-10-25 13:09:15 +0000

    Add NULL check in for loop

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(cur_node);
 		return (1);
 	}
-	for (i = 0; i <= index; i++)
+	for (i = 0; i <= index && cur_node != NULL; i++)
 	{
 		if (i == index)
 		{
