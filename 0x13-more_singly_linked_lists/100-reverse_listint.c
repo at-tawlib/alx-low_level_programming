2f1443e00b57ad882a4b5102ae1fa653ed690710
at-tawlib <a.fatahu95@gmail.com>
2022-10-25 16:21:38 +0000

    Add 100-reverse_lintint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: head of list
+ * Return: reverse list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev_node, *cur_node;
+
+	prev_node = NULL;
+	while (*head != NULL)
+	{
+		cur_node = *head;
+		*head = (*head)->next;
+		cur_node->next = prev_node;
+		prev_node = cur_node;
+	}
+	*head = cur_node;
+	return (*head);
+}
