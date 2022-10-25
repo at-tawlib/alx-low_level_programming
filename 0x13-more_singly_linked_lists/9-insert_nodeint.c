435dd686b8e66db9c0e44846112288f101737327
at-tawlib <a.fatahu95@gmail.com>
2022-10-25 07:08:43 +0000

    Handle code in index 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 
+	/** if node is added at the beginning (0 index) **/
 	if (idx == 0)
 	{
-		new_node->next = (*head)->next;
+		new_node->next = *head;
 		*head = new_node;
 		return (*head);
 	}
