9e3d09480ab22744df65c0230ef465907ac43c36
at-tawlib <a.fatahu95@gmail.com>
2022-10-26 02:39:57 +0000

    Remove last code and change if statement

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
-	if (*head == NULL || head == NULL)
+	if (head == NULL)
 	{
 		return;
 	}
@@ -18,5 +18,4 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = node;
 	}
-	free(node);
 }
