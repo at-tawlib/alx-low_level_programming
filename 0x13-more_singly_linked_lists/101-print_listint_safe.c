470f33f95ad47e03da2c7fc97993da3210fdff84
at-tawlib <a.fatahu95@gmail.com>
2022-10-26 03:38:49 +0000

    Add 101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints linked list (safe version)
+ * @head: head of list
+ * Return: number of nodes in list
+ *
+ * Description - checks if addresses
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	long int diff;
+
+	while (head)
+	{
+		/** check diff of mem addresses */
+		diff = head - head->next;
+		printf("%lu\n", diff);
+		count++;
+		printf("[%p] %d\n", (void *) head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			/** prints address and data of next node */
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+	return (count);
+}
