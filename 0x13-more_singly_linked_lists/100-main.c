b578a961242fec439c21b0f62728a2ea37d1b616
at-tawlib <a.fatahu95@gmail.com>
2022-10-27 23:53:28 +0000

    Add main.c files

--- 0x13-more_singly_linked_lists/100-main.c
+++ 0x13-more_singly_linked_lists/100-main.c
@@ -13,9 +13,6 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
-	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 0, 523);
-	print_listint(head);
 	add_nodeint_end(&head, 0);
 	add_nodeint_end(&head, 1);
 	add_nodeint_end(&head, 2);
@@ -25,17 +22,8 @@ int main(void)
 	add_nodeint_end(&head, 402);
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
-	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 5, 4096);
-	print_listint(head);
-	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 0, 523);
-	print_listint(head);
-	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 30, 4096);
-	print_listint(head);
-	printf("-----------------\n");
-	insert_nodeint_at_index(&head, -1, -25);
+	reverse_listint(&head);
+	printf("\n-----------------------\n");
 	print_listint(head);
 	free_listint2(&head);
 	return (0);
