87f94492349e30179ddc3744a8f91bee8596770f
at-tawlib <a.fatahu95@gmail.com>
2022-10-27 01:40:25 +0000

    Change for statement (check null for prev_node)

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	cur_node = *head;
 	prev_node = *head;
 	/** traverse to position */
-	for (i = 0; i <= idx && cur_node != NULL; i++)
+	for (i = 0; i <= idx && prev_node != NULL; ++i)
 	{
 		if (i == idx)
 		{
