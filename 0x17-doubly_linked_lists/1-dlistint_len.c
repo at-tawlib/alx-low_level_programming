ceb2074bd3be01bedb96eacec084246b25c4884e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-12-09 02:38:51 +0000

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len: returns the number of elements in a linked dlistint_t
+ * @h: head of list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
